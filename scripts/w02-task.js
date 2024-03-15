39cf084c21b8159626a089610542d7adea3ba220
JLKohl <121449936+JLKohl@users.noreply.github.com>
2024-03-15 11:07:12 -0400

    stuff

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -14,7 +14,7 @@ let otherFood = 'cake';
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector("#year");
-const imageElement = document.querySelector('img')
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 
